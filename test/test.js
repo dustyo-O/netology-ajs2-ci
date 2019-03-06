a69ab39db1460dc0722d08b33b4927728c66cee4
Alexandr Shleyko <dusty@yandex-team.ru>
2019-03-06 20:57:04 +0300

    new tests

--- test/test.js
+++ test/test.js
@@ -1,4 +1,32 @@
-import { getHtmlTag } from '../src/js/script';
+import { calculateTotal, getHtmlTag } from '../src/js/script';
+
+test('should calculate total for purchases', () => {
+  const input = [
+    {
+      id: 1, name: '...', price: 33, count: 3,
+    }, {
+      id: 2, name: '...', price: 55, count: 2,
+    },
+  ];
+
+  const expected = 209;
+  const received = calculateTotal(input);
+
+  expect(received).toBe(expected);
+});
+
+test('should calculate total for purchases', () => {
+  const input = [
+    {
+      id: 1, name: '...', price: 33, count: 3,
+    },
+  ];
+
+  const expected = 99;
+  const received = calculateTotal(input);
+
+  expect(received).toBe(expected);
+});
 
 test('should make full tag', () => {
   const expected = '<div></div>';
